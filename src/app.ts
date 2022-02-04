e06b1487f0978dfb1f8cc09e411f828ab392a691
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-02-04 19:11:46 +0900

    getters & setters

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 class Department {
   // private name: string;
-  private employees: string[] = [];
+  protected employees: string[] = [];
 
   constructor(private readonly id: string, public name: string) {
     // this.name = n;
@@ -27,16 +27,41 @@ class ITDepartment extends Department {
 }
 
 class AccountingDepartment extends Department {
+  private lastReport: string;
+
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+    throw new Error("No report found.");
+  }
+
+  set mostRecentReport(value: string) {
+    if (!value) {
+      throw new Error("Please pass in a valid value!");
+    }
+    this.addReport(value);
+  }
+
   constructor(id: string, private reports: string[]) {
     super(id, "Accounting");
+    this.lastReport = reports[0];
   }
 
   addReport(text: string) {
     this.reports.push(text);
+    this.lastReport = text;
   }
 
   printReports() {
     console.log(this.reports);
+  }
+
+  addEmployee(name: string) {
+    if (name === "Max") {
+      return;
+    }
+    this.employees.push(name);
   }
 }
 
@@ -45,8 +70,15 @@ console.log(itDep);
 console.log(itDep.admins);
 
 const accounting = new AccountingDepartment("d2", []);
+
+accounting.mostRecentReport = "test lastest report";
+
 accounting.addReport("Somthinge went wrong...");
+accounting.mostRecentReport = "123";
+console.log(accounting.mostRecentReport);
 accounting.printReports();
+accounting.addEmployee("ryan");
+accounting.printEmployeeInformation();
 // const accounting = new Department("1", "Accounting");
 // accounting.describe();
 
