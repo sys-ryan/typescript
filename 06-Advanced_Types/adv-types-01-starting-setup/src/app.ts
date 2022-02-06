8e94c728601276f3a2097643e8adce2f17743461
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-02-06 18:22:57 +0900

    More on type guards

--- 06-Advanced_Types/adv-types-01-starting-setup/src/app.ts
+++ 06-Advanced_Types/adv-types-01-starting-setup/src/app.ts
@@ -1,41 +1,41 @@
-//using type
-// type Admin = {
-//   name: string;
-//   privileges: string[];
-// };
-
-// type Employee = {
-//   name: string;
-//   startDate: Date;
-// };
-
-// type ElevatedEmployee = Admin & Employee; //intersection
-
-// const e1: ElevatedEmployee = {
-//   name: "Max",
-//   privileges: ["create-server"],
-//   startDate: new Date(),
-// };
-
-// using interface
-interface Admin {
+// -  using type
+type Admin = {
   name: string;
   privileges: string[];
-}
+};
 
-interface Employee {
+type Employee = {
   name: string;
   startDate: Date;
-}
+};
 
-interface ElevatedEmployee extends Employee, Admin {}
-// type ElevatedEmployee = Admin & Employee; //intersection
+type ElevatedEmployee = Admin & Employee; //intersection
 
 const e1: ElevatedEmployee = {
-  name: "max",
+  name: "Max",
   privileges: ["create-server"],
   startDate: new Date(),
 };
+
+//-  using interface
+// interface Admin {
+//   name: string;
+//   privileges: string[];
+// }
+
+// interface Employee {
+//   name: string;
+//   startDate: Date;
+// }
+
+// interface ElevatedEmployee extends Employee, Admin {}
+// // type ElevatedEmployee = Admin & Employee; //intersection
+
+// const e1: ElevatedEmployee = {
+//   name: "max",
+//   privileges: ["create-server"],
+//   startDate: new Date(),
+// };
 
 // --------
 
@@ -43,3 +43,63 @@ type Combinable = string | number;
 type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
+
+// Type guards
+
+// - type guards using 'typeof'
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+
+  return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log("Name: " + emp.name);
+
+  if ("privileges" in emp) {
+    console.log("Privileges: " + emp.privileges);
+  }
+
+  if ("startDate" in emp) {
+    console.log("StartDate: " + emp.startDate);
+  }
+}
+
+printEmployeeInformation(e1);
+printEmployeeInformation({ name: "Menu", startDate: new Date() });
+
+// - typeguard with 'instanceof'
+class Car {
+  drive() {
+    console.log("Driving...");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("Driving a truck...");
+  }
+
+  loadCargo(amount: number) {
+    console.log("Loading cargo ..." + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
