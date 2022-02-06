911afe0ba342874ef6381a29ef9a00bd4cfeec06
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-02-06 19:30:40 +0900

    nullish coalescing

--- 06-Advanced_Types/adv-types-05-type-casting/src/app.ts
+++ 06-Advanced_Types/adv-types-05-type-casting/src/app.ts
@@ -45,6 +45,11 @@ const fetchedUserData = {
 
 console.log(fetchedUserData?.job?.title);
 
+const userInput = "";
+const storedData = userInput ?? "DEFAULT";
+// ?? if it is 'null' or 'undefined' then ~
+console.log(storedData);
+
 type UnknownEmployee = Employee | Admin;
 
 function printEmployeeInformation(emp: UnknownEmployee) {
