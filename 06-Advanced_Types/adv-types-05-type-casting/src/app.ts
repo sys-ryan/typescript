b41c7e10891667880d2d77ab70e39b56c071c968
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-02-06 19:27:57 +0900

    Optional Chaining

--- 06-Advanced_Types/adv-types-05-type-casting/src/app.ts
+++ 06-Advanced_Types/adv-types-05-type-casting/src/app.ts
@@ -37,6 +37,14 @@ function add(a: Combinable, b: Combinable) {
 const result = add(1, "hello");
 result.split("");
 
+const fetchedUserData = {
+  id: "u1",
+  name: "Max",
+  job: { title: "CEO", description: "My own company" },
+};
+
+console.log(fetchedUserData?.job?.title);
+
 type UnknownEmployee = Employee | Admin;
 
 function printEmployeeInformation(emp: UnknownEmployee) {
