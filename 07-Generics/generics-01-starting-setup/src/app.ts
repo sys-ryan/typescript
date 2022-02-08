5248ee80994ebafc71d5ca19fb626dbbfd89a823
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-02-08 14:17:59 +0900

    creating a generic function

--- 07-Generics/generics-01-starting-setup/src/app.ts
+++ 07-Generics/generics-01-starting-setup/src/app.ts
@@ -2,8 +2,15 @@
 // const names = ["ryan", "kim"];
 const names: Array<string> = []; // string[]
 
-const promise: Promise<string> = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("This is done!");
-  }, 2000);
-});
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("This is done!");
+//   }, 2000);
+// });
+
+function merge<T, U>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const mergedObj = merge({ name: "ryan" }, { age: 29 });
+console.log(mergedObj.age);
