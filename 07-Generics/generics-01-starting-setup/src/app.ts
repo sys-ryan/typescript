0aea56ace1319839ccee0abb0409375259bffab7
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-02-08 14:26:18 +0900

    another generic function

--- 07-Generics/generics-01-starting-setup/src/app.ts
+++ 07-Generics/generics-01-starting-setup/src/app.ts
@@ -14,3 +14,19 @@ function merge<T extends object, U extends object>(objA: T, objB: U) {
 
 const mergedObj = merge({ name: "ryan" }, { age: 29 });
 console.log(mergedObj.age);
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = "Got no value.";
+
+  if (element.length === 1) {
+    descriptionText = "Got 1 eleemnt.";
+  } else if (element.length > 1) {
+    descriptionText = "Got " + element.length + " elements";
+  }
+  return [element, descriptionText];
+}
+console.log(countAndDescribe("Hi there"));
