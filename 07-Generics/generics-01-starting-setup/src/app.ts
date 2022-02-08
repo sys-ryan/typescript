9b85f10c228617af770dfa0f113f6c65cfb7904c
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-02-08 14:20:46 +0900

    constrains

--- 07-Generics/generics-01-starting-setup/src/app.ts
+++ 07-Generics/generics-01-starting-setup/src/app.ts
@@ -8,7 +8,7 @@ const names: Array<string> = []; // string[]
 //   }, 2000);
 // });
 
-function merge<T, U>(objA: T, objB: U) {
+function merge<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 
