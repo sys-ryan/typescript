b0bdb8098597539bc84969b95472bab1d7e8e03a
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-02-15 00:25:18 +0900

    import&export syntexes

--- 09-Drag_and_Drop_Project/prj-00-initial-starting-setup/src/components/project-input.ts
+++ 09-Drag_and_Drop_Project/prj-00-initial-starting-setup/src/components/project-input.ts
@@ -1,6 +1,8 @@
 import { Component } from "./base-component.js";
-import { Validatable, validate } from "../util/validation.js";
-import { autobind } from "../decorators/autobind.js";
+// import { Validatable, validate } from "../util/validation.js";
+import * as Validation from "../util/validation.js"; //import as a group
+// import { autobind } from "../decorators/autobind.js";
+import { autobind as Autobind } from "../decorators/autobind.js"; // using alias
 import { projectState } from "../state/project-state.js";
 
 // ProjectInput Class
@@ -30,24 +32,24 @@ export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
     const enteredDescription = this.descriptionInputElement.value;
     const enteredPeople = this.peopleInputElement.value;
 
-    const titleValidatable: Validatable = {
+    const titleValidatable: Validation.Validatable = {
       value: enteredTitle,
       required: true,
     };
 
-    const descriptionValidatable: Validatable = {
+    const descriptionValidatable: Validation.Validatable = {
       value: enteredDescription,
       required: true,
       minLength: 5,
     };
 
-    const peopleValidatable: Validatable = {
+    const peopleValidatable: Validation.Validatable = {
       value: +enteredPeople,
       required: true,
       min: 1,
     };
 
-    if (!validate(titleValidatable) || !validate(descriptionValidatable) || !validate(peopleValidatable)) {
+    if (!Validation.validate(titleValidatable) || !Validation.validate(descriptionValidatable) || !Validation.validate(peopleValidatable)) {
       alert("Invalid input, please try again!");
       return;
     } else {
@@ -61,7 +63,7 @@ export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
     this.peopleInputElement.value = "";
   }
 
-  @autobind
+  @Autobind
   private submitHandler(event: Event) {
     event.preventDefault();
     const userInput = this.getherUserInput();
