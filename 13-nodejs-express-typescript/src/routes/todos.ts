25c1a55239e01dec105fec95e594b53db793fd42
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-02-16 16:18:50 +0900

    13: CRUD Operations

--- 13-nodejs-express-typescript/src/routes/todos.ts
+++ 13-nodejs-express-typescript/src/routes/todos.ts
@@ -1,13 +1,15 @@
 import { Router } from "express";
 
+import { createTodo, getTodos, updateTodo, deleteTodo } from "../controllers/todos";
+
 const router = Router();
 
-router.post("/");
+router.post("/", createTodo);
 
-router.get("/");
+router.get("/", getTodos);
 
-router.patch("/:id");
+router.patch("/:id", updateTodo);
 
-router.delete("/:id");
+router.delete("/:id", deleteTodo);
 
 export default router;
