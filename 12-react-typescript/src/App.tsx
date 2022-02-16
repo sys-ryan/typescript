33ad7562e4ac9b1ba63d2a846db64162c4220257
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-02-16 10:45:17 +0900

    12: delete todo

--- 12-react-typescript/src/App.tsx
+++ 12-react-typescript/src/App.tsx
@@ -11,10 +11,16 @@ const App: React.FC = () => {
     setTodos((prevTodos) => [...prevTodos, { id: Math.random().toString(), text }]);
   };
 
+  const todoDeleteHandler = (todoId: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== todoId);
+    });
+  };
+
   return (
     <div className="App">
       <NewTodo onAddTodo={todoAddHandler} />
-      <TodoList items={todos} />
+      <TodoList items={todos} onDeleteTodo={todoDeleteHandler} />
     </div>
   );
 };
